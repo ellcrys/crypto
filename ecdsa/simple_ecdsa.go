2b27752a3e5031240e1390d5258e845b3ddbc617
Kennedy Idialu <kennedyidialu@gmail.com>
2017-01-08 11:51:27 +0100

    Include simple ecdsa helper structure.
    Replace godep with glide.

---
+++ ecdsa/simple_ecdsa.go
@@ -0,0 +1,170 @@
+package ecdsa
+
+// Simple ECDSA makes available convenient
+// methods to allow simple and less technical
+// use of Elliptic curve encryption. For example,
+// The use of ampersand as a separator when concantenating
+// x and y coordinates or for the r and s signature pairs.
+//
+// Examples
+//
+// ecd := NewSimpleECDSA()
+// ecd.GenerateKey()
+// pubKey, _ := LoadPubKey(ecd.GetPubKey())
+// fmt.Println("Private Key: ", ecd.GetPrivKey())
+// signature, _ := ecd.Sign(rand.Reader, []byte("hello"))
+// verified := Verify(pubKey, []byte("hello"), []byte(signature))
+
+import (
+	goecdsa "crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"math/big"
+	"strings"
+)
+
+const (
+	CurveP256 = "p256"
+)
+
+// SimpleECDSA defines code for creating ECDSA keys,
+// signing and verifying data.
+type SimpleECDSA struct {
+	curve   elliptic.Curve
+	privKey *goecdsa.PrivateKey
+}
+
+// NewSimpleECDSA creates a new SimpleECDSA object
+func NewSimpleECDSA(curveName string) *SimpleECDSA {
+	var curve elliptic.Curve
+	switch curveName {
+	case CurveP256:
+		curve = elliptic.P256()
+	default:
+		panic("unsupported elliptic curve")
+	}
+	var se = &SimpleECDSA{curve: curve}
+	if err := se.GenerateKey(); err != nil {
+		panic(err)
+	}
+	return se
+}
+
+// LoadPubKey creates a public key object from compact
+// x and y cordinates contantenated by an ampersand.
+func LoadPubKey(pubKey string, curveName string) (*goecdsa.PublicKey, error) {
+
+	pubKeySplit := strings.Split(pubKey, "&")
+	if len(pubKeySplit) != 2 {
+		return nil, errors.New("invalid public key. public key must have x and y coordinates")
+	}
+
+	x := pubKeySplit[0]
+	y := pubKeySplit[1]
+
+	xByte, err := hex.DecodeString(x)
+	if err != nil {
+		return nil, errors.New("failed to decode x coordinate")
+	}
+
+	yByte, err := hex.DecodeString(y)
+	if err != nil {
+		return nil, errors.New("failed to decode y coordinate")
+	}
+
+	var curve elliptic.Curve
+	switch curveName {
+	case CurveP256:
+		curve = elliptic.P256()
+	default:
+		return nil, errors.New("unsupported elliptic curve")
+	}
+
+	return &goecdsa.PublicKey{
+		Curve: curve,
+		X:     new(big.Int).SetBytes(xByte),
+		Y:     new(big.Int).SetBytes(yByte),
+	}, nil
+}
+
+// GetPubKey gets the compact version of the public key
+// created in the current instance.
+// of a 1-byte descriptor indicating the uncompressed
+// form of the public key
+func (se *SimpleECDSA) GetPubKey() string {
+	x := se.privKey.Public().(*goecdsa.PublicKey).X
+	y := se.privKey.Public().(*goecdsa.PublicKey).Y
+	return fmt.Sprintf("%x&%x", x.Bytes(), y.Bytes())
+}
+
+// GetPrivKey returns the private key
+func (se *SimpleECDSA) GetPrivKey() string {
+	return fmt.Sprintf("%x", se.privKey.D)
+}
+
+// GenerateKey creates a new ECDSA private key
+func (se *SimpleECDSA) GenerateKey() error {
+
+	privKey, err := goecdsa.GenerateKey(se.curve, rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	se.privKey = privKey
+	return nil
+}
+
+// Sign a byte slice. Return the a signature
+// comprising of the r and s pairs concantenated by an
+// ampersand character
+func (se *SimpleECDSA) Sign(rand io.Reader, hashed []byte) (string, error) {
+	r, s, err := goecdsa.Sign(rand, se.privKey, hashed)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s&%s", hex.EncodeToString(r.Bytes()), hex.EncodeToString(s.Bytes())), nil
+}
+
+// IsValidCompactPubKey checks whether the public key
+// is a valid compact public key format.
+func IsValidCompactPubKey(pubKey string) (bool, error) {
+	if len(strings.Split(pubKey, "&")) != 2 {
+		return false, errors.New("invalid public key. public key must have x and y coordinates")
+	}
+	return true, nil
+}
+
+// Verify a signature. Expects a signature composed of
+// the Elliptic curve r and s pairs concantenated by an
+// ampersand character (&).
+func Verify(pubKey *goecdsa.PublicKey, hash []byte, sig []byte) error {
+
+	// get r and s individual values from signature
+	sigSplit := strings.Split(string(sig), "&")
+	if len(sigSplit) != 2 {
+		return errors.New("invalid signed hash. expects r and s coordinates joined by an ampersand")
+	}
+
+	rByte, err := hex.DecodeString(sigSplit[0])
+	if err != nil {
+		return errors.New("failed to hex decode on r value")
+	}
+
+	sByte, err := hex.DecodeString(sigSplit[1])
+	if err != nil {
+		return errors.New("failed to hex decode on s value")
+	}
+
+	// verify signature
+	if goecdsa.Verify(
+		pubKey, hash,
+		new(big.Int).SetBytes(rByte),
+		new(big.Int).SetBytes(sByte)) {
+		return nil
+	}
+	return errors.New("verification failed")
+}
---
+++ ecdsa/simple_ecdsa_test.go
@@ -0,0 +1,174 @@
+package ecdsa
+
+import (
+	"testing"
+
+	"strings"
+
+	"crypto/rand"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+// TestSimpleECDSASpec test ../simple_ecdsa.go
+func TestSimpleECDSASpec(t *testing.T) {
+
+	Convey("SimpleECDSA", t, func() {
+
+		Convey(".NewSimpleECDSA()", func() {
+
+			Convey("should panic if called with an unsupported elliptic curve", func() {
+				So(func() { NewSimpleECDSA("p224") }, ShouldPanicWith, "unsupported elliptic curve")
+			})
+
+			Convey("should not panic if called with a supported elliptic curve", func() {
+				So(func() { NewSimpleECDSA("p256") }, ShouldNotPanic)
+			})
+
+		})
+
+		Convey(".GetPubKey()", func() {
+			Convey("should successfully return an ampersand delimited public key", func() {
+				key := NewSimpleECDSA(CurveP256)
+				pubFormatted := key.GetPubKey()
+				So(pubFormatted, ShouldNotBeNil)
+			})
+		})
+
+		Convey(".GenerateKey()", func() {
+			Convey("should successfully generate a private key", func() {
+				key := NewSimpleECDSA(CurveP256)
+				currentKey := key.GetPrivKey()
+				err := key.GenerateKey()
+				So(err, ShouldBeNil)
+				So(currentKey, ShouldNotEqual, key.GetPrivKey())
+			})
+		})
+
+		Convey(".GetPrivKey()", func() {
+			Convey("should successfully return a private key", func() {
+				key := NewSimpleECDSA(CurveP256)
+				privKey := key.GetPrivKey()
+				So(privKey, ShouldNotBeNil)
+			})
+		})
+
+		Convey(".LoadPubKey()", func() {
+
+			Convey("should fail if public key format is invalid", func() {
+				pk, err := LoadPubKey("wrong", CurveP256)
+				So(pk, ShouldBeNil)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "invalid public key. public key must have x and y coordinates")
+			})
+
+			Convey("should fail if public key x cordinate is invalid", func() {
+				pk, err := LoadPubKey("wrong_x&some_y", CurveP256)
+				So(pk, ShouldBeNil)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "failed to decode x coordinate")
+			})
+
+			Convey("should fail if public key y cordinate is invalid", func() {
+				key := NewSimpleECDSA(CurveP256)
+				pubFormatted := key.GetPubKey()
+				pubKeyParts := strings.Split(pubFormatted, "&")
+				pk, err := LoadPubKey(pubKeyParts[0]+"&some_y", CurveP256)
+				So(pk, ShouldBeNil)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "failed to decode y coordinate")
+			})
+
+			Convey("should fail if elliptic curve is unsupported", func() {
+				key := NewSimpleECDSA(CurveP256)
+				pubFormatted := key.GetPubKey()
+				pk, err := LoadPubKey(pubFormatted, "p224")
+				So(pk, ShouldBeNil)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "unsupported elliptic curve")
+			})
+
+			Convey("should successfully load an ampersand formatted public key", func() {
+				key := NewSimpleECDSA(CurveP256)
+				pubFormatted := key.GetPubKey()
+				pk, err := LoadPubKey(pubFormatted, CurveP256)
+				So(pk, ShouldNotBeNil)
+				So(err, ShouldBeNil)
+			})
+		})
+
+		Convey(".IsValidCompactPubKey", func() {
+
+			Convey("should fail if public key is not a valid compact/formatted key", func() {
+				valid, err := IsValidCompactPubKey("wrong")
+				So(valid, ShouldEqual, false)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "invalid public key. public key must have x and y coordinates")
+			})
+
+			Convey("should return success if public key format is valid", func() {
+				key := NewSimpleECDSA(CurveP256)
+				pubFormatted := key.GetPubKey()
+				valid, err := IsValidCompactPubKey(pubFormatted)
+				So(valid, ShouldEqual, true)
+				So(err, ShouldBeNil)
+			})
+		})
+
+		Convey(".Sign()", func() {
+
+			Convey("should successfully sign text", func() {
+				key := NewSimpleECDSA(CurveP256)
+				s, err := key.Sign(rand.Reader, []byte("hello"))
+				So(err, ShouldBeNil)
+				So(s, ShouldNotBeEmpty)
+			})
+		})
+
+		Convey(".Verify()", func() {
+
+			key := NewSimpleECDSA(CurveP256)
+			s, err := key.Sign(rand.Reader, []byte("hello"))
+			So(err, ShouldBeNil)
+
+			Convey("should fail if signed hash is invalid", func() {
+				key := NewSimpleECDSA(CurveP256)
+				pubKey := key.privKey.PublicKey
+				err := Verify(&pubKey, []byte("wrong"), []byte("wrong"))
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "invalid signed hash. expects r and s coordinates joined by an ampersand")
+			})
+
+			Convey("should fail if r cordinate could not be decoded", func() {
+				key := NewSimpleECDSA(CurveP256)
+				pubKey := key.privKey.PublicKey
+				err := Verify(&pubKey, []byte("wrong"), []byte("wrong_r&wrong_s"))
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "failed to hex decode on r value")
+			})
+
+			Convey("should fail if s cordinate could not be decoded", func() {
+				key := NewSimpleECDSA(CurveP256)
+				pubKey := key.privKey.PublicKey
+				sigParts := strings.Split(s, "&")
+				err := Verify(&pubKey, []byte("wrong"), []byte(sigParts[0]+"&wrong_s"))
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "failed to hex decode on s value")
+			})
+
+			Convey("should fail if signature could not be verified", func() {
+				key := NewSimpleECDSA(CurveP256)
+				pubKey := key.privKey.PublicKey
+				err := Verify(&pubKey, []byte("hi"), []byte(s))
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "verification failed")
+			})
+
+			Convey("should successfully verify signature", func() {
+				pubKey := key.privKey.PublicKey
+				err := Verify(&pubKey, []byte("hello"), []byte(s))
+				So(err, ShouldBeNil)
+			})
+		})
+	})
+}
